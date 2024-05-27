5e0f27f62b8b5391a48a9e3234eb8fcd43436f0a
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2024-05-27 11:51:43 -0300

    correcciones

--- src/app.js
+++ src/app.js
@@ -3,13 +3,21 @@ import morgan from 'morgan';
 import officesRouter from './routes/areas.routes.js';
 import tonersRoutes from './routes/toners.routes.js';
 import ordersRoutes from './routes/orders.routes.js'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials:true
+}))
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api',officesRouter)
 app.use('/api',tonersRoutes)
 app.use('/api', ordersRoutes)
+
+
 
 export default app
