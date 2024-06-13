b58c9d10ffcb0549884c83974d5a8e80b66d4c40
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2024-06-13 12:53:23 -0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import app from "./app.js"
 import  connectDB  from "./db.js"
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT || 3500
 
 connectDB()
-app.listen(3500)
-console.log("servidor en el puerto", 3500)
+app.listen(port, () => {
+    console.log(`servidor en el puerto ${port}`)
+  })
